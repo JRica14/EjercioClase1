5245d800f1a6a5aeb355e26d61189c3789846441
Jorge <jrrr@carlos.es>
2022-04-20 10:11:37 +0200

    clase 20/04/2022 principio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,26 +5,31 @@ public class Main {
 
         public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
-                int array []= new int [10];
+                int array[] = new int[10];
 
-                for (int i=0; i<10; i++){
-                        array[i]=Integer.parseInt(sc.nextLine());
+                for (int i = 0; i < 10; i++) {
+                        array[i] = Integer.parseInt(sc.nextLine());
                 }
-                int cont=0;
+                contarRepeticiones(array);
+        }
+
+
+        public static void contarRepeticiones(int[] array) {
+                int cont = 0;
                 Arrays.sort(array);
                 int num = array[0];
-                cont=0;
-                System.out.print("{" );
+                cont = 0;
+                System.out.print("{");
                 for (int i = 0; i < 10; i++) {
-                        if(num==array[i])
+                        if (num == array[i])
                                 cont++;
-                        else{
-                                System.out.print(num + "=" + cont+ ", ");
-                                num=array[i];
-                                cont=1;
+                        else {
+                                System.out.print(num + "=" + cont + ", ");
+                                num = array[i];
+                                cont = 1;
                         }
                 }
                 System.out.print(num + "=" + cont);
-                System.out.print("}" );
+                System.out.print("}");
         }
 }
